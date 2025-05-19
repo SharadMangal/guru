4d99b4d4c6e26dec3454f8e2558b5e3cae777423
SharadMangal <114740965+SharadMangal@users.noreply.github.com>
2025-05-19 16:28:42 +0530

    Remove secrets.dart from tracking

--- lib/secrets.dart
+++
@@ -1,3 +0,0 @@
-// const openAIAPIKey = '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';
-
-const openAIAPIKey = String.fromEnvironment('OPENAI_API_KEY');
